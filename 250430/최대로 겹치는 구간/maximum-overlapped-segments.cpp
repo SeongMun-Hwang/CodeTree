c99ffc2b4b2ffc6bbb7d0134a6680f08d2c3b926
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-04-30 16:18:03 +0900

    Title: 최대로 겹치는 구간, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250430/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 250430/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int n;
 int x1[100], x2[100];
-int block[101]={0};
+int block[201]={0};
 int main() {
     cin >> n;
     int max=0;
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
-        for(int j=x1[i];j<x2[i]; j++){
+        for(int j=x1[i]+100;j<x2[i]+100; j++){
             block[j]++;
             if(block[j]>max) max = block[j];
         }
