ded6c9f65de551097b63edcacaeb6201793eedb4
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-04-30 16:41:01 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250430/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 250430/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -5,29 +5,33 @@ using namespace std;
 int n;
 int x[100];
 char dir[100];
-int block[100];
+int block[101];  // 크기를 101로 변경, 인덱스 0부터 100까지 사용할 수 있도록
+
 int main() {
     cin >> n;
-    int index=49;
+    int index = 50;  // index는 50부터 시작
+
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> dir[i];
-        if(dir[i]=='R'){
-            for(int j=index;j<x[i]+index;j++){
+        if (dir[i] == 'R') {
+            for (int j = index; j < index + x[i]; j++) {
                 block[j]++;
             }
-            index+=x[i];
+            index += x[i];
         }
-        else if(dir[i]=='L'){
-            for(int j=index-1;j>=index-x[i];j--){
-                block[j]++;            
+        else if (dir[i] == 'L') {
+            for (int j = index - 1; j >= index - x[i]; j--) {
+                block[j]++;
             }
-            index-=x[i];
+            index -= x[i];
         }
     }
-    int count=0;
-    for(int i=0;i<101;i++){
-        if(block[i]>=2) count++;
+
+    int count = 0;
+    for (int i = 0; i < 101; i++) {
+        if (block[i] >= 2) count++;
     }
+
     cout << count;
     return 0;
 }
