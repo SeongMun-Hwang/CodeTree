531ad9c16f3b3987f63a4d335c19ac42292dab34
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-04-30 16:43:45 +0900

    Title: 왔다 갔던 구역 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250430/왔다 갔던 구역 2/area-been-to-and-from2.cpp
+++ 250430/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -5,35 +5,37 @@ using namespace std;
 int n;
 int x[100];
 char dir[100];
-int block[101];  // 크기 101로 수정
+int block[101];  // 범위가 0~100이므로 크기를 101로 설정
 
 int main() {
     cin >> n;
-    int index = 50;  // index는 50부터 시작
+    int index = 50;  // 처음 시작 위치는 50
 
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> dir[i];
+        
         if (dir[i] == 'R') {
             // 오른쪽으로 이동
             for (int j = index; j < index + x[i]; j++) {
                 block[j]++;
             }
-            index += x[i];
+            index += x[i];  // index를 오른쪽으로 이동
         }
         else if (dir[i] == 'L') {
             // 왼쪽으로 이동
             for (int j = index - 1; j >= index - x[i]; j--) {
                 block[j]++;
             }
-            index -= x[i];
+            index -= x[i];  // index를 왼쪽으로 이동
         }
     }
 
+    // 겹치는 부분을 세는 부분
     int count = 0;
     for (int i = 0; i < 101; i++) {
-        if (block[i] >= 2) count++;  // 2개 이상 겹치는 구간을 카운트
+        if (block[i] >= 2) count++;  // 겹치는 부분은 2 이상
     }
 
-    cout << count;
+    cout << count;  // 최종적으로 겹치는 부분의 개수를 출력
     return 0;
 }
