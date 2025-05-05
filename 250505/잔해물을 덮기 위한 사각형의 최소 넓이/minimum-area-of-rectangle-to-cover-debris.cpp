665cec252c3ad7bc72d0cccbd49f3269e691af06
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-05-05 13:44:52 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 14ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250505/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 250505/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
+int x1[2], y1[2];
+int x2[2], y2[2];
+int arr[2000][2000];
 int main() {
-    int x1, y1, x2, y2;
-    int a1, b1, a2, b2;
-    cin >> x1 >> y1 >> x2 >> y2;
-    cin >> a1 >> b1 >> a2 >> b2;
-
-    int area = (x2 - x1) * (y2 - y1);
-    int result = area;
-
-    // 세로로 완전히 막을 때
-    if (a1 <= x1 && a2 >= x2) {
-        int top = max(0, y2 - b2);
-        int bottom = max(0, b1 - y1);
-        result = min(top, bottom) * (x2 - x1);
+    cin >> x1[0] >> y1[0] >> x2[0] >> y2[0];
+    cin >> x1[1] >> y1[1] >> x2[1] >> y2[1];
+    x1[0]+=1000;
+    y1[0]+=1000;
+    x2[0]+=1000;
+    y2[0]+=1000;
+    x1[1]+=1000;
+    y1[1]+=1000;
+    x2[1]+=1000;
+    y2[1]+=1000;
+    for(int n = 0; n<2;n++){
+        for(int i=x1[n]; i<=x2[n]; i++){
+            for(int j=y1[n]; j<=y2[n]; j++){
+                if(n==0) arr[i][j]=1;
+                else arr[i][j]=0;
+            }
+        }
+    }
+    int min_x=2000, min_y=2000;
+    int max_x=0, max_y=0;
+    for(int i=0;i<2000;i++){
+        for(int j=0;j<2000;j++){
+            if(arr[i][j]==1){
+                if(i>max_x) max_x=i;
+                if(i<min_x) min_x=i;
+                if(j>max_y) max_y=j;
+                if(j<min_y) min_y=j; 
+            }
+        }
     }
 
-    // 가로로 완전히 막을 때
-    if (b1 <= y1 && b2 >= y2) {
-        int right = max(0, x2 - a2);
-        int left = max(0, a1 - x1);
-        result = min(right, left) * (y2 - y1);
-    }
+    cout << (max_x-min_x) * (max_y-min_y);
 
-    cout << result;
     return 0;
 }
