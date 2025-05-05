b49349d163aaff1717964946d9da65eae09a4b50
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-05-05 14:04:28 +0900

    Title: 계속 중첩되는 사각형, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250505/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
+++ 250505/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -10,8 +10,8 @@ int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
-        for(int j=x1[i];j<x2[i];j++){
-            for(int k=y1[i];k<y2[i];k++){
+        for(int j=x1[i]+100;j<x2[i]+100;j++){
+            for(int k=y1[i]+100;k<y2[i]+100;k++){
                 if(i%2==0) arr[j][k]='R';
                 else arr[j][k]='B';
             }
