a2860559a0d83d578ab38bd16f811937b4ee4f56
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-05-05 14:03:35 +0900

    Title: 계속 중첩되는 사각형, Time: 15ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250505/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x1[10], y1[10];
+int x2[10], y2[10];
+char arr[200][200];
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+        for(int j=x1[i];j<x2[i];j++){
+            for(int k=y1[i];k<y2[i];k++){
+                if(i%2==0) arr[j][k]='R';
+                else arr[j][k]='B';
+            }
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<200;i++){
+        for(int j=0;j<200;j++){
+            if(arr[i][j]=='B') cnt++;
+        }
+    }
+    cout << cnt;
+    // Please write your code here.
+
+    return 0;
+}
