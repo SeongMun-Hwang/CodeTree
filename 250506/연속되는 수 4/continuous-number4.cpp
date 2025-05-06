b77cf4a153eb31f3e15a13faba7648be7c6eeb8c
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-05-06 16:23:07 +0900

    Title: 연속되는 수 4, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250506/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    int cnt = 1;
+    int max = 1;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+
+        if(i!=0){
+            if(arr[i]>arr[i-1]){
+                cnt++;
+            }
+            else{
+                cnt=1;
+            }
+        }
+        if(cnt>max) max=cnt;
+    }
+    cout << max;
+    return 0;
+}
